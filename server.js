2902076e8e8d61d0917340dec0e990fe120c0c68
shivam-gupta-ttn <shivam.gupta@tothenew.com>
2021-04-14 16:36:43 +0530

    added delete and did required changes

--- server.js
+++ server.js
@@ -1,38 +1,51 @@
 const Express = require('express');
-const PORT = 5000;
-const app = Express();
+
 const fs = require('fs');
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const data = fs.readFileSync('./users.json')
-let users = JSON.parse(data);
+
+const PORT = 5000;
+const app = Express();
 
 app.use(cors())
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use((req,res,next)=>{
+
+app.use((req, res, next) => {
     req.requestTime = Date.now();
-    next(); 
+    next();
 })
+
 app.use(bodyParser.json());
 
-app.get('/users',(req,res)=>{
+app.get('/users', (req, res) => {
+    const data = fs.readFileSync('./users.json')
+    let users = JSON.parse(data);
     res.send(users)
-    
+
 })
-app.post('/login', (req,res)=>{
-    let users = req.body;
-    users["key"]= req.requestTime 
-    // console.log(req.body)
-    // console.log(req.requestTime)
-    console.log(users);
-    users = JSON.stringify(users)
-    fs.appendFile('./users.json',users, (err,data)=>{
-        if (err) throw err
-        console.log("Appending file completed");
-    })
+
+app.delete('/delete', (req, res) => {
+    console.log("received")
+    console.log(req.body)
+    const data = JSON.parse(fs.readFileSync('./users.json'));
+
+    const updatedUsers = data.filter((e, i) => (i !== req.body.payload))
+    fs.writeFileSync('./users.json', JSON.stringify(updatedUsers, null, 2))
+    res.send({ success: true })
 })
-app.listen(PORT,()=>{
-    console.log("Server is running @http://localhost:%d",PORT);
+
+app.post('/register', (req, res) => {
+    const data = JSON.parse(fs.readFileSync('./users.json'));
+
+    let fetchedUser = req.body;
+    fetchedUser["Created_on"] = req.requestTime
+    data.push(fetchedUser)
+    fs.writeFileSync('./users.json', JSON.stringify(data, null, 2));
+    res.send({ data: "success" })
+
+})
+app.listen(PORT, () => {
+    console.log("Server is running @http://localhost:%d", PORT);
 })
